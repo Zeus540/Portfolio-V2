f1c8bc782b17b84f567d23577daec183fc99ded1
Zaheer <zaheerroberts4@gmail.com>
2021-01-19 13:17:42 +0200

    ran build

---
+++ build/precache-manifest.927f05de05386b6674b178a1caf7f060.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afb882ad3051aa10c8aaccf9ed53efd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d256caeea1599c57de4",
+    "url": "/static/css/2.5c8bfc1c.chunk.css"
+  },
+  {
+    "revision": "5762eae044134f6e14dc",
+    "url": "/static/css/main.738156dc.chunk.css"
+  },
+  {
+    "revision": "4d256caeea1599c57de4",
+    "url": "/static/js/2.663e8810.chunk.js"
+  },
+  {
+    "revision": "5762eae044134f6e14dc",
+    "url": "/static/js/main.684834b5.chunk.js"
+  },
+  {
+    "revision": "21dcdbd52874846b3891",
+    "url": "/static/js/runtime-main.1add1218.js"
+  },
+  {
+    "revision": "4b4a06d57edd5a09df86fa2834478f94",
+    "url": "/static/media/Crud.4b4a06d5.png"
+  },
+  {
+    "revision": "d2c6b51e1b17990333049d5ca7329a3d",
+    "url": "/static/media/FunFox.d2c6b51e.png"
+  },
+  {
+    "revision": "dff015aff53072a8e4b52e1638b061dd",
+    "url": "/static/media/Kkwe.dff015af.png"
+  },
+  {
+    "revision": "4d23cd4f50cd8a8744b04d7d62f9d5a6",
+    "url": "/static/media/Nike.4d23cd4f.png"
+  },
+  {
+    "revision": "285758872c45dbf41afa214e10541281",
+    "url": "/static/media/Port.28575887.png"
+  },
+  {
+    "revision": "061370d68bcfe9a571437a6f04b584dd",
+    "url": "/static/media/Rot.061370d6.png"
+  },
+  {
+    "revision": "128072fd6e853f66c6413298154924de",
+    "url": "/static/media/cv.128072fd.pdf"
+  },
+  {
+    "revision": "43f367734a24e319c212e85be64413b6",
+    "url": "/static/media/me3.43f36773.png"
+  }
+]);
